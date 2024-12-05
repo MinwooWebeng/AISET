79edebcf94bd8be09754616712ea9a3c0ae1622d
songminwoo1 <mwsong@webeng.kaist.ac.kr>
2024-12-05 18:02:34 +0900

    rr

--- mallang_ga.py
+++ mallang_ga.py
@@ -14,13 +14,14 @@ TOURNAMENT_SIZE = 4 # Tournament selection의 selection set 크기
 
 MAX_GENERATIONS = 100   # 최대 실행 세대
 MUTATION_RATE = 0.9     # crossover 이후에 각각의 개체가 mutation을 겪을 확률
-GENE_MUTATION_RATE = 0.1    # mutation을 겪는 개체에서, 각각의 유전자가 mutate 될 확률
-GENE_MUTATION_STRENGTH = 0.05   # mutation 되는 유전자에서, gaussian distribution 으로 값을 +- 하는데, 그 distribution의 mean 값
+GENE_MUTATION_RATE = 0.2    # mutation을 겪는 개체에서, 각각의 유전자가 mutate 될 확률
+GENE_MUTATION_STRENGTH = 0.03   # mutation 되는 유전자에서, gaussian distribution 으로 값을 +- 하는데, 그 distribution의 mean 값
 
-INPUT_AUDIO = "./datasets/EGFxDataset/clean_neck/1-0.wav"   # 원본 오디오
-TARGET_AUDIO = "./datasets/EGFxDataset/BD2_neck/1-0.wav"    # 변조된 오디오 (목표)
+INPUT_AUDIO = "./datasets/10s_datasets/clean/audio_Ballade_4.wav"   # 원본 오디오
+TARGET_AUDIO = "./datasets/10s_datasets/Dynamics/processed_audio_6.wav"    # 변조된 오디오 (목표)
 
-RESULT_FOLDER = "experiment_2"  # 각 세대별 최선을 모을 폴더
+RESULT_FOLDER = "experiment_5"  # 각 세대별 최선을 모을 폴더
+TEMP_FOLDER = "experiment_5/temp"
 
 i_name = 0  # 개체 이름 (파일 생성시에 구분용)
 class Individual:   # 유전자 (float array) 랑 fitness (float), 이름
@@ -36,11 +37,11 @@ class Individual:   # 유전자 (float array) 랑 fitness (float), 이름
 
     def evaluate(self):
         processed_audio, sr = maudio.FX_to_Audio(self.genome, INPUT_AUDIO)  # 현재 유전자 대로 오디오 변조
-        sf.write("temp/" + str(self.i_name) + "_temp_audio.wav", processed_audio, sr)   # 변조된 파일 작성
-        fq_fitness, _, _, _ = fitness.fitness_frequency_domain("temp/" + str(self.i_name) + "_temp_audio.wav", TARGET_AUDIO)    # frequency domain 유사도
-        tm_fitness, _, _, _ = fitness.fitness_time_domain("temp/" + str(self.i_name) + "_temp_audio.wav", TARGET_AUDIO) # time domain 유사도
+        sf.write(TEMP_FOLDER + "/" + str(self.i_name) + "_temp_audio.wav", processed_audio, sr)   # 변조된 파일 작성
+        fq_fitness, _, _, _ = fitness.fitness_frequency_domain(TEMP_FOLDER + "/" + str(self.i_name) + "_temp_audio.wav", TARGET_AUDIO)    # frequency domain 유사도
+        tm_fitness, _, _, _ = fitness.fitness_time_domain(TEMP_FOLDER + "/" + str(self.i_name) + "_temp_audio.wav", TARGET_AUDIO) # time domain 유사도
         self.fitness = 0.5 * fq_fitness + 0.5 - 5 * (1 - tm_fitness)    # 유사도 합치기 (비율 조정)
-        os.remove("temp/" + str(self.i_name) + "_temp_audio.wav")   # 변조된 파일 삭제
+        os.remove(TEMP_FOLDER + "/" + str(self.i_name) + "_temp_audio.wav")   # 변조된 파일 삭제
         
     def mutate(self):
         for i in range(len(self.genome)):
